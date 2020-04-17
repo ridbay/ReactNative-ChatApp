d37858ae4537083925477c8562a3bbe66200a3d6
Ridwan Balogun <balogunridwan@gmail.com>
2020-04-17 09:09:34 +0100

    React Native Chat App

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <PaperProvider>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      </PaperProvider>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
